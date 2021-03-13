84679764e3347b5578092a581693e65b422ea445
skol-coder <63770798+skol-coder@users.noreply.github.com>
2021-03-13 22:43:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const url = 'mongodb+srv://student_dba:student_dba@cluster0.vtcph.mongodb.net/StudentDB?retryWrites=true&w=majority';
-let port = process.env.PORT || 9000;
+let port = process.env.PORT || 5000;
 
 const app = express();
 
 
-let bodyParser = require('body-parser');
-app.use(
-    bodyParser.urlencoded({
-      extended: false
-    })
-  );
-  app.use(bodyParser.json());
+// let bodyParser = require('body-parser');
+// app.use(
+//     bodyParser.urlencoded({
+//       extended: false
+//     })
+//   );
+//   app.use(bodyParser.json());
   
 
 mongoose.connect(url, {useNewUrlParser:true});
