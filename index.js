dd40a34f607a4748d8b7f3dc2b7ce7c3cda292ec
skol-coder <63770798+skol-coder@users.noreply.github.com>
2021-03-13 18:50:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const url = 'mongodb+srv://student_dba:student_dba@cluster0.vtcph.mongodb.net/StudentDB?retryWrites=true&w=majority';
+let port = process.env.PORT || 9000;
+
+const app = express();
+
+
+let bodyParser = require('body-parser');
+app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
+  
+
+mongoose.connect(url, {useNewUrlParser:true});
+
+const con = mongoose.connection;
+con.on('open',function(){
+    console.log('connected....');
+});
+
+app.use(express.json());
+
+const routeto = require('./routers/students');
+app.use('/students',routeto);
+
+
+app.listen(port, function(){
+    console.log('Server started....');
+});
