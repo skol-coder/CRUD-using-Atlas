7c23489a84bf1b3249e266e2db1ae5ed3759b42d
skol-coder <63770798+skol-coder@users.noreply.github.com>
2021-03-17 22:40:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ let port = process.env.PORT || 5000;
 const app = express();
 
 
+//CORS
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+
 let bodyParser = require('body-parser');
 app.use(
     bodyParser.urlencoded({
       extended: false
     })
   );
-  app.use(bodyParser.json());
+app.use(bodyParser.json());
   
 
 mongoose.connect(url, {useNewUrlParser:true});
